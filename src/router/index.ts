e841173b338f850e6969458ee1fcea26c377c81c
FatPaper-1874 <360655753@qq.com>
2024-04-15 10:27:11 +0800

    将路由修改成懒路由，解决打包js文件过大问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,32 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import loginVue from "@/views/login/login.vue";
-import roomListVue from "@/views/hall/room-list.vue";
-import roomVue from "@/views/room/room.vue";
-import gameVue from "@/views/game/game.vue";
-import { useUserInfo } from "@/store";
+import {createRouter, createWebHashHistory} from "vue-router";
+import {useUserInfo} from "@/store";
 
 const routes = [
-	{ path: "/", name: "login", component: loginVue },
-	{ path: "/room-list", name: "room-list", component: roomListVue },
-	{ path: "/room", name: "room", component: roomVue },
-	{ path: "/game", name: "game", component: gameVue },
+    {path: "/", name: "login", component: () => import('@/views/login/login.vue')},
+    {path: "/room-list", name: "room-list", component: () => import('@/views/hall/room-list.vue')},
+    {path: "/room", name: "room", component: () => import('@/views/room/room.vue')},
+    {path: "/game", name: "game", component: () => import('@/views/game/game.vue')},
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
-	routes,
+    history: createWebHashHistory(),
+    routes,
 });
 
 router.beforeEach((to, form) => {
-	if (
-		// 检查用户是否已登录
-		!localStorage.getItem("token") &&
-		//  避免无限重定向
-		to.name !== "login"
-	) {
-		// 将用户重定向到登录页面
-		return { name: "login" };
-	}
-	const user = useUserInfo();
-	if (!user.userId && to.name !== "login" && to.name !== "room-list") {
-		return { name: "room-list" };
-	}
+    if (
+        // 检查用户是否已登录
+        !localStorage.getItem("token") &&
+        //  避免无限重定向
+        to.name !== "login"
+    ) {
+        // 将用户重定向到登录页面
+        return {name: "login"};
+    }
+    const user = useUserInfo();
+    if (!user.userId && to.name !== "login" && to.name !== "room-list") {
+        return {name: "room-list"};
+    }
 });
 
 export default router;
