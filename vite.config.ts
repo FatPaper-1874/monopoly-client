d01f6a501709bea1e6a1c0c5305fc471626b11e3
FatPaper-1874 <360655753@qq.com>
2024-04-17 00:50:23 +0800

    优化打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import {visualizer} from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
+import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		viteCompression(),
+		visualizer({
+			gzipSize: true,
+			brotliSize: true,
+			emitFile: false,
+			filename: "test.html", //分析图生成的文件名
+			open: true //如果存在本地服务端口，将在打包后自动展示
+		})
+	],
+	build:{
+		rollupOptions: {
+			plugins: [
+				externalGlobals({
+					vue: "Vue",
+				}),
+			],
+		},
+	},
 	resolve: {
 		alias: [
 			{
