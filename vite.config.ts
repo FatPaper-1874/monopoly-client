81cc726e4978f35427b914ac6448b65d85a26b4a
FatPaper-1874 <360655753@qq.com>
2024-04-17 12:02:43 +0800

    优化打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import {visualizer} from 'rollup-plugin-visualizer';
@@ -7,37 +7,38 @@ import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		viteCompression(),
-		visualizer({
-			gzipSize: true,
-			brotliSize: true,
-			emitFile: false,
-			filename: "test.html", //分析图生成的文件名
-			open: true //如果存在本地服务端口，将在打包后自动展示
-		})
-	],
-	build:{
-		rollupOptions: {
-			plugins: [
-				externalGlobals({
-					vue: "Vue",
-					three: "THREE",
-					gsap: "gsap"
-				}),
-			],
-		},
-	},
-	resolve: {
-		alias: [
-			{
-				find: "@",
-				replacement: path.resolve(path.dirname("./"), "src"),
-			},
-		],
-	},
-	server: {
-		port: 7000,
-	},
+    plugins: [
+        vue(),
+        viteCompression(),
+        visualizer({
+            gzipSize: true,
+            brotliSize: true,
+            emitFile: false,
+            filename: "test.html", //分析图生成的文件名
+            open: true //如果存在本地服务端口，将在打包后自动展示
+        })
+    ],
+    build: {
+        rollupOptions: {
+            plugins: [
+                externalGlobals({
+                    vue: "Vue",
+                    'vue-router': 'VueRouter ',
+                    three: "THREE",
+                    gsap: "gsap"
+                }),
+            ],
+        },
+    },
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: path.resolve(path.dirname("./"), "src"),
+            },
+        ],
+    },
+    server: {
+        port: 7000,
+    },
 });
