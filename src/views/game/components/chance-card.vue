a468138bd1ea42af72e678dd751fc4dccb76dad2
FatPaper-1874 <360655753@qq.com>
2024-07-11 16:45:13 +0800

    为机会卡添加icon图片

--- src/views/game/components/chance-card.vue
+++ src/views/game/components/chance-card.vue
@@ -1,79 +1,73 @@
 <script setup lang="ts">
-import { findIconDefinition, IconName } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { computed } from "vue";
-import { ChanceCardInfo } from '@/interfaces/bace';
+import {computed, watch} from "vue";
 
-const props = defineProps<{ chanceCard: ChanceCardInfo, disable: boolean }>();
-const chanceCard = props.chanceCard;
-
-const legalIconName = computed(() => {
-	if (findIconDefinition({ prefix: "fas", iconName: chanceCard.icon as IconName })) {
-		return chanceCard.icon;
-	} else {
-		return "circle-question";
-	}
+const props = defineProps({
+  id: {type: String, default: ""},
+  name: {type: String, default: ""},
+  describe: {type: String, default: ""},
+  icon: {type: String, default: ""},
+  color: {type: String, default: "rgb(255, 187, 50)"},
 });
+
+const iconUrl = computed(() => {
+  return props.icon.includes("http") ? props.icon : `http://${props.icon}`;
+})
 </script>
 
 <template>
-	<div v-chanceCardSource="chanceCard" class="chance-card" :style="{ border: `0.4em solid ${chanceCard.color}` }"
-		:class="{ disable }">
-		<div class="icon" :style="{ 'color': chanceCard.color }">
-			<FontAwesomeIcon :icon="['fas', 'circle-question']"></FontAwesomeIcon>
-		</div>
-		<div class="name" :style="{ 'color': chanceCard.color }">{{ chanceCard.name }}</div>
-		<div class="describe" :style="{ 'color': chanceCard.color }">{{ chanceCard.describe }}</div>
-	</div>
+  <div class="chance-card" :style="{ border: `0.4em solid ${props.color}` }">
+    <div class="icon" v-if="icon"><img :src="iconUrl" alt=""/></div>
+    <div class="name" :style="{ color }">{{ name }}</div>
+    <div class="describe" :style="{ color: props.color }">{{ describe }}</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .chance-card {
-	min-width: 9rem;
-	min-height: 12rem;
-	width: 9rem;
-	height: 12rem;
-	font-size: 0.8rem;
-	background-color: #ffffff;
-	box-sizing: border-box;
-	border-radius: 20px;
-	box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
-	user-select: none;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	overflow: hidden;
-	transition: .3s;
-	cursor: pointer;
+  font-size: 16px;
+  min-width: 10.5em;
+  min-height: 14em;
+  width: 10.5em;
+  height: 14em;
+  background-color: #ffffff;
+  box-sizing: border-box;
+  border-radius: 1.3em;
+  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
 
-	&.disable {
-		filter: grayscale(1);
-		pointer-events: none;
-		cursor: not-allowed;
-	}
+  & > .icon {
+    margin-bottom: 0.8em;
 
-	&>.icon {
-		font-size: 3.4em;
-		margin-top: 0.4em;
-		margin-bottom: 0.3em;
-	}
+    & > img {
+      $img-size: 3.6em;
+      width: $img-size;
+      height: $img-size;
+    }
+  }
 
-	&>.name {
-		font-size: 1.6em;
-		margin-bottom: 0.7em;
-	}
+  & > .name {
+    font-size: 1.3em;
+    font-weight: 700;
+    margin-bottom: 0.8em;
+  }
 
-	&>.describe {
-		width: 80%;
-		font-size: 1em;
-		margin-bottom: 1em;
-		word-wrap: break-word;
-		overflow-y: scroll;
-		text-align: center;
+  & > .describe {
+    width: 80%;
+    font-weight: 700;
+    font-size: 0.75em;
+    margin-bottom: 1em;
+    word-wrap: break-word;
+    overflow-y: scroll;
+    text-align: center;
 
-		&::-webkit-scrollbar {
-			display: none;
-		}
-	}
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
 }
 </style>
