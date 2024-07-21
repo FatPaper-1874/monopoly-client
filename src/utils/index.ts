cd4923ccd5d2de0e56463ca9e67ef6be5afbc80e
FatPaper-1874 <360655753@qq.com>
2024-07-21 14:37:45 +0800

    修改登录页面调出方式为iframe

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,153 +1,167 @@
 import * as THREE from "three";
 
 export const lightenColor = (hexColor: string, amount: number): string => {
-	// Remove the '#' character from the beginning of the string
-	hexColor = hexColor.slice(1);
+    // Remove the '#' character from the beginning of the string
+    hexColor = hexColor.slice(1);
 
-	// Convert the hex color to RGB
-	const r = parseInt(hexColor.slice(0, 2), 16);
-	const g = parseInt(hexColor.slice(2, 4), 16);
-	const b = parseInt(hexColor.slice(4, 6), 16);
+    // Convert the hex color to RGB
+    const r = parseInt(hexColor.slice(0, 2), 16);
+    const g = parseInt(hexColor.slice(2, 4), 16);
+    const b = parseInt(hexColor.slice(4, 6), 16);
 
-	// Calculate the new RGB values by decreasing each by the specified amount
-	const newR = Math.max(r - amount, 0);
-	const newG = Math.max(g - amount, 0);
-	const newB = Math.max(b - amount, 0);
+    // Calculate the new RGB values by decreasing each by the specified amount
+    const newR = Math.max(r - amount, 0);
+    const newG = Math.max(g - amount, 0);
+    const newB = Math.max(b - amount, 0);
 
-	// Convert the new RGB values back to hex
-	const newHexColor = `#${newR.toString(16).padStart(2, "0")}${newG.toString(16).padStart(2, "0")}${newB
-		.toString(16)
-		.padStart(2, "0")}`;
+    // Convert the new RGB values back to hex
+    const newHexColor = `#${newR.toString(16).padStart(2, "0")}${newG.toString(16).padStart(2, "0")}${newB
+        .toString(16)
+        .padStart(2, "0")}`;
 
-	return newHexColor;
+    return newHexColor;
 };
 
 export const getRandomInteger = (min: number, max: number) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 export function arrayBufferToImgUrl(buffer: number[]): string {
-	return (
-		"data:image/png;base64," +
-		window.btoa(new Uint8Array(buffer).reduce((res, byte) => res + String.fromCharCode(byte), ""))
-	);
+    return (
+        "data:image/png;base64," +
+        window.btoa(new Uint8Array(buffer).reduce((res, byte) => res + String.fromCharCode(byte), ""))
+    );
 }
 
 export function shuffleArray<T>(arr: T[]) {
-	return arr.sort(function () {
-		return 0.5 - Math.random();
-	});
+    return arr.sort(function () {
+        return 0.5 - Math.random();
+    });
 }
 
 export function randomString(length: number) {
-	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	let randomString = "";
-	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * characters.length);
-		randomString += characters.charAt(randomIndex);
-	}
-	return randomString;
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let randomString = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+    return randomString;
 }
 
 export function throttle(fn: Function, interval: number, first = false) {
-	let lastTime = first ? 0 : Date.now();
+    let lastTime = first ? 0 : Date.now();
 
-	return function () {
-		if (Date.now() >= interval + lastTime) {
-			lastTime = Date.now();
-			fn(...arguments);
-		}
-	};
+    return function () {
+        if (Date.now() >= interval + lastTime) {
+            lastTime = Date.now();
+            fn(...arguments);
+        }
+    };
 }
 
 export function debounce(fn: Function, delay_ms: number) {
-	let timer: NodeJS.Timeout;
-	return function () {
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			fn(...arguments);
-		}, delay_ms);
-	};
+    let timer: NodeJS.Timeout;
+    return function () {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...arguments);
+        }, delay_ms);
+    };
 }
 
 export function getScreenPosition(object3D: THREE.Object3D, camera: THREE.Camera) {
-	if (!object3D) return new THREE.Vector2();
-	const tempPosition = new THREE.Vector3(0, 0, 0);
-	object3D.getWorldPosition(tempPosition);
-	tempPosition.project(camera);
-	return new THREE.Vector2(tempPosition.x, -tempPosition.y);
+    if (!object3D) return new THREE.Vector2();
+    const tempPosition = new THREE.Vector3(0, 0, 0);
+    object3D.getWorldPosition(tempPosition);
+    tempPosition.project(camera);
+    return new THREE.Vector2(tempPosition.x, -tempPosition.y);
 }
 
 export async function requestFullScreen() {
-	const element = document.documentElement;
-	if (element.requestFullscreen) {
-		await element.requestFullscreen();
-		//@ts-ignore
-	} else if (element.mozRequestFullScreen) {
-		// 兼容Firefox
-		//@ts-ignore
-		await element.mozRequestFullScreen();
-		//@ts-ignore
-	} else if (element.webkitRequestFullscreen) {
-		// 兼容Chrome和Safari
-		//@ts-ignore
-		await element.webkitRequestFullscreen();
-	}
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+        await element.requestFullscreen();
+        //@ts-ignore
+    } else if (element.mozRequestFullScreen) {
+        // 兼容Firefox
+        //@ts-ignore
+        await element.mozRequestFullScreen();
+        //@ts-ignore
+    } else if (element.webkitRequestFullscreen) {
+        // 兼容Chrome和Safari
+        //@ts-ignore
+        await element.webkitRequestFullscreen();
+    }
 }
 
 export async function exitFullScreen() {
-	if (isFullScreen()) {
-		if (document.exitFullscreen) {
-			await document.exitFullscreen();
-			//@ts-ignore
-		} else if (document.mozCancelFullScreen) {
-			// 兼容Firefox
-			//@ts-ignore
-			await document.mozCancelFullScreen();
-			//@ts-ignore
-		} else if (document.webkitExitFullscreen) {
-			// 兼容Chrome和Safari
-			//@ts-ignore
-			await document.webkitExitFullscreen();
-		}
-	}
+    if (isFullScreen()) {
+        if (document.exitFullscreen) {
+            await document.exitFullscreen();
+            //@ts-ignore
+        } else if (document.mozCancelFullScreen) {
+            // 兼容Firefox
+            //@ts-ignore
+            await document.mozCancelFullScreen();
+            //@ts-ignore
+        } else if (document.webkitExitFullscreen) {
+            // 兼容Chrome和Safari
+            //@ts-ignore
+            await document.webkitExitFullscreen();
+        }
+    }
 }
 
 export function setTimeOutAsync(delay: number = 1000, fn?: Function) {
-	return new Promise((reslove) => {
-		setTimeout(() => {
-			if (fn) fn();
-			reslove("");
-		}, delay);
-	});
+    return new Promise((reslove) => {
+        setTimeout(() => {
+            if (fn) fn();
+            reslove("");
+        }, delay);
+    });
 }
 
 export function isMobileDevice() {
-	return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
 export function isFullScreen() {
-	return document.fullscreenElement !== null;
+    return document.fullscreenElement !== null;
 }
 
 export function isLandscape() {
-	return window.matchMedia("(orientation: landscape)").matches;
+    return window.matchMedia("(orientation: landscape)").matches;
 }
 
-export function hexToRgbNormalized(hex:string) {
-	// Remove the hash at the start if it's there
-	hex = hex.replace(/^#/, '');
+export function hexToRgbNormalized(hex: string) {
+    // Remove the hash at the start if it's there
+    hex = hex.replace(/^#/, '');
 
-	// Parse the r, g, b values
-	const bigint = parseInt(hex, 16);
-	const r = (bigint >> 16) & 255;
-	const g = (bigint >> 8) & 255;
-	const b = bigint & 255;
+    // Parse the r, g, b values
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
 
-	// Convert to 0-1 range
-	const rNormalized = r / 255;
-	const gNormalized = g / 255;
-	const bNormalized = b / 255;
+    // Convert to 0-1 range
+    const rNormalized = r / 255;
+    const gNormalized = g / 255;
+    const bNormalized = b / 255;
 
-	return { r: rNormalized, g: gNormalized, b: bNormalized };
+    return {r: rNormalized, g: gNormalized, b: bNormalized};
+}
+
+export function createLoginIframeOnBody(url: string): Promise<string> {
+    const iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.id = "login-iframe";
+    document.body.appendChild(iframe);
+    return new Promise((resolve, reject) => {
+        window.addEventListener('message', e => {
+            const token = e.data;
+            document.body.removeChild(iframe);
+            resolve(token);
+        })
+    })
 }
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,39 +1,31 @@
 <script setup lang="ts">
-import { ref, onBeforeMount, onBeforeUnmount, reactive, onMounted, nextTick } from "vue";
+import {ref, onBeforeMount, onBeforeUnmount, reactive, onMounted, nextTick} from "vue";
 import router from "@/router";
-import { __LOGINPAGEURL__ } from "../../../global.config";
-import { getUserByToken } from "@/utils/api/user";
-import { exitFullScreen, setTimeOutAsync } from "@/utils";
+import {__LOGINPAGEURL__} from "../../../global.config";
+import {getUserByToken} from "@/utils/api/user";
+import {createLoginIframeOnBody, exitFullScreen, setTimeOutAsync} from "@/utils";
 import FPMessage from "@/components/utils/fp-message/index";
-import { useUserInfo } from "@/store";
-import { LoginCodeRenderer } from "@/utils/three/login-code-renderer";
+import {useUserInfo} from "@/store";
+import {LoginCodeRenderer} from "@/utils/three/login-code-renderer";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import FpPopover from "@/components/utils/fp-popover/fp-popover.vue";
 
 let loginCodeRenderer: LoginCodeRenderer | null;
 let diceRotate: boolean = true;
 const firstClick = ref(false);
 
-onBeforeMount(async () => {});
-
-function doLogin() {
-	firstClick.value = true;
-	const url = new URL(window.location.href);
-	const _URLSearchParams = new URLSearchParams(location.search);
-	const tokenFromURL = _URLSearchParams.get("token") || "";
-	const tokenFromLocal = localStorage.getItem("token") || "";
-	if (!tokenFromURL && !tokenFromLocal) {
-		toLogin();
-	} else {
-		let token = tokenFromLocal;
-		if (tokenFromURL) {
-			localStorage.setItem("token", tokenFromURL);
-			token = tokenFromURL;
-		}
-		nextTick(() => {
-			const canvasEl = document.getElementById("dice-canvas") as HTMLCanvasElement;
-			loginCodeRenderer = new LoginCodeRenderer(canvasEl, diceRotate);
-			getUserInfoToRoomList(token);
-		});
-	}
+async function doLogin() {
+  firstClick.value = true;
+  let token = localStorage.getItem("token") || "";
+  if (!token) {
+    token = await toLogin();
+    localStorage.setItem("token", token)
+  }
+  nextTick(() => {
+    const canvasEl = document.getElementById("dice-canvas") as HTMLCanvasElement;
+    loginCodeRenderer = new LoginCodeRenderer(canvasEl, diceRotate);
+    getUserInfoToRoomList(token);
+  });
 }
 
 // onMounted(() => {
@@ -42,162 +34,230 @@ function doLogin() {
 // });
 
 onBeforeUnmount(() => {
-	loginCodeRenderer && loginCodeRenderer.clear();
-	loginCodeRenderer = null;
+  loginCodeRenderer && loginCodeRenderer.clear();
+  loginCodeRenderer = null;
 });
 
 async function getUserInfoToRoomList(token: string) {
-	try {
-		const { id: userId, useraccount, username, avatar, color } = await getUserByToken(token);
-		const userInfoStore = useUserInfo();
-		userInfoStore.$patch({ userId, useraccount, username, avatar, color });
-		await setTimeOutAsync(1500);
-		if (loginCodeRenderer) await loginCodeRenderer.showImage(`http://${avatar}`);
-		await setTimeOutAsync(2000, toRoomList);
-	} catch (e: any) {
-		FPMessage({
-			type: "error",
-			message: e || e.message || "在验证身份时发生了未知的错误",
-			onClosed: () => {
-				toLogin();
-			},
-		});
-	}
+  try {
+    const {id: userId, useraccount, username, avatar, color} = await getUserByToken(token);
+    const userInfoStore = useUserInfo();
+    userInfoStore.$patch({userId, useraccount, username, avatar, color});
+    await setTimeOutAsync(1500);
+    if (loginCodeRenderer) await loginCodeRenderer.showImage(`http://${avatar}`);
+    await setTimeOutAsync(2000, toRoomList);
+  } catch (e: any) {
+    FPMessage({
+      type: "error",
+      message: e || e.message || "在验证身份时发生了未知的错误",
+      onClosed: () => {
+        doLogin();
+        loginCodeRenderer && loginCodeRenderer.clear();
+        loginCodeRenderer = null;
+      },
+    });
+  }
 }
 
 async function toLogin() {
-	await exitFullScreen();
-	location.assign(__LOGINPAGEURL__);
+  await exitFullScreen();
+  return await createLoginIframeOnBody(__LOGINPAGEURL__);
 }
 
 function toRoomList() {
-	if (localStorage.getItem("token")) {
-		router.replace({ name: "room-list" });
-	}
+  if (localStorage.getItem("token")) {
+    router.replace({name: "room-list"});
+  }
 }
 </script>
 
 <template>
-	<div @click.once="doLogin" class="login-page">
-		<div class="title">
-			<span>FatPaper的大富翁</span>
-		</div>
-		<div class="login-code-container">
-			<div class="tip" v-show="!firstClick">点击任意位置继续</div>
-			<!-- <img class="login-code loading" :src="imgUrl" alt=""> -->
-			<canvas id="dice-canvas" class="dice" v-show="firstClick"></canvas>
-		</div>
-	</div>
+  <div @click.once="doLogin" class="login-page">
+    <div class="title">
+      <span>FatPaper的大富翁</span>
+    </div>
+    <div class="login-code-container">
+      <div class="tip" v-show="!firstClick">点击任意位置继续</div>
+      <!-- <img class="login-code loading" :src="imgUrl" alt=""> -->
+      <canvas id="dice-canvas" class="dice" v-show="firstClick"></canvas>
+    </div>
+
+    <div class="login-extra">
+      <fp-popover placement="right" trigger="hover">
+        <div class="login-extra-item about">
+          <FontAwesomeIcon icon="bullhorn"/>
+        </div>
+        <template #content>
+          <div class="extra-content">查看公告</div>
+        </template>
+      </fp-popover>
+      <fp-popover placement="right" trigger="hover">
+        <div class="login-extra-item about">
+          <FontAwesomeIcon icon="bug"/>
+        </div>
+        <template #content>
+          <div class="extra-content">有Bug？扫码提交</div>
+        </template>
+      </fp-popover>
+      <fp-popover placement="right" trigger="hover">
+        <div class="login-extra-item to-bilibili">Bili</div>
+        <template #content>
+          <div class="extra-content">ss</div>
+        </template>
+      </fp-popover>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .login-page {
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .login-code-container {
-	width: 100%;
-	height: 100%;
-	flex: 1;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin-bottom: 5rem;
+  width: 100%;
+  height: 100%;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5rem;
 
-	& > .tip {
-		font-size: 2rem;
-		margin-top: auto;
-		margin-bottom: 3rem;
-		color: #ffffff;
-		text-shadow: var(--text-shadow);
-		animation: blink infinite 1.8s;
-		cursor: pointer;
+  & > .tip {
+    font-size: 2rem;
+    margin-top: auto;
+    margin-bottom: 3rem;
+    color: #ffffff;
+    text-shadow: var(--text-shadow);
+    animation: blink infinite 1.8s;
+    cursor: pointer;
 
-		@keyframes blink {
-			0% {
-				opacity: 1;
-			}
-			50% {
-				opacity: 0.3;
-			}
-			100% {
-				opacity: 1;
-			}
-		}
-	}
+    @keyframes blink {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0.3;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
 
-	.dice {
-		$img_size: 28rem;
+  .dice {
+    $img_size: 28rem;
 
-		width: $img_size;
-		height: $img_size;
-		user-select: none;
-	}
+    width: $img_size;
+    height: $img_size;
+    user-select: none;
+  }
 }
 
 .title {
-	margin-top: 30px;
+  margin-top: 30px;
 
-	& > span {
-		font-size: 6em;
-		color: #ffffff;
-		letter-spacing: 0.1em;
-		display: block;
-		position: relative;
-		user-select: none;
+  & > span {
+    font-size: 6em;
+    color: #ffffff;
+    letter-spacing: 0.1em;
+    display: block;
+    position: relative;
+    user-select: none;
 
-		&::before,
-		&::after {
-			content: "FatPaper的大富翁";
-		}
+    &::before,
+    &::after {
+      content: "FatPaper的大富翁";
+    }
 
-		&:before,
-		&:after {
-			position: absolute;
-			left: 0;
-			top: 0;
-		}
+    &:before,
+    &:after {
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
 
-		&:before {
-			color: #ff9114;
-			z-index: -1;
-			animation: rotate1 5s ease-in-out infinite;
-		}
+    &:before {
+      color: #ff9114;
+      z-index: -1;
+      animation: rotate1 5s ease-in-out infinite;
+    }
 
-		&:after {
-			color: #7e7e7e;
-			z-index: -2;
-			animation: rotate2 5s ease-in-out infinite;
-		}
-	}
+    &:after {
+      color: #7e7e7e;
+      z-index: -2;
+      animation: rotate2 5s ease-in-out infinite;
+    }
+  }
 
-	@keyframes rotate1 {
-		0%,
-		100% {
-			-webkit-transform: translate3d(3px, 3px, 3px);
-			transform: translate3d(3px, 3px, 3px);
-		}
+  @keyframes rotate1 {
+    0%,
+    100% {
+      -webkit-transform: translate3d(3px, 3px, 3px);
+      transform: translate3d(3px, 3px, 3px);
+    }
 
-		50% {
-			-webkit-transform: translate3d((-3px, 3px, -3px));
-			transform: translate3d((-3px, 3px, -3px));
-		}
-	}
+    50% {
+      -webkit-transform: translate3d((-3px, 3px, -3px));
+      transform: translate3d((-3px, 3px, -3px));
+    }
+  }
 
-	@keyframes rotate2 {
-		0%,
-		100% {
-			-webkit-transform: translate3d(5px, 5px, 5px);
-			transform: translate3d(5px, 5px, 5px);
-		}
+  @keyframes rotate2 {
+    0%,
+    100% {
+      -webkit-transform: translate3d(5px, 5px, 5px);
+      transform: translate3d(5px, 5px, 5px);
+    }
 
-		50% {
-			-webkit-transform: translate3d((-5px, 5px, -5px));
-			transform: translate3d((-5px, 5px, -5px));
-		}
-	}
+    50% {
+      -webkit-transform: translate3d((-5px, 5px, -5px));
+      transform: translate3d((-5px, 5px, -5px));
+    }
+  }
+}
+
+.login-extra {
+  position: absolute;
+  left: 2rem;
+  bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  .login-extra-item {
+    width: 5rem;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1rem 0;
+    background-color: rgba(255, 255, 255, .7);
+    font-size: 2rem;
+    border-radius: .7rem;
+    color: var(--color-second);
+    text-shadow: var(--text-shadow);
+    cursor: pointer;
+    box-shadow: var(--box-shadow);
+
+    & > * {
+      filter: drop-shadow(3px 3px 2px rgba(0, 0, 0, 0.15));
+    }
+  }
+
+  .extra-content {
+    width: 5rem;
+    margin-left: 1rem;
+    font-size: 1.2rem;
+    background-color: rgba(255, 255, 255, .7);
+    border-radius: .7rem;
+    padding: .6rem;
+    color: var(--color-primary);
+    text-shadow: var(--text-shadow);
+  }
 }
 </style>
